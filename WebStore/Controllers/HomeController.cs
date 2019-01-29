ec1d395af7b8e7f1e1033362e186dd24ab015e70
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-29 21:18:55 +0300

    Представление списка сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,9 +9,21 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<EmployeeViewModel> __Employes = new List<EmployeeViewModel>
+        {
+            new EmployeeViewModel { Id = 0, FirstName = "Иван", SecondName = "Иванов", Patronymic = "Иванович", Age = 28 },
+            new EmployeeViewModel { Id = 0, FirstName = "Пётр", SecondName = "Петров", Patronymic = "Петрович", Age = 35 },
+            new EmployeeViewModel { Id = 0, FirstName = "Сидор", SecondName = "Сидоров", Patronymic = "Сидорович", Age = 22 },
+        };
+
+        public HomeController()
+        {
+
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(__Employes);
         }  
     }
 }
