399e3483c61e8ff068bb8eba043905bf169b9f4a
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-01 20:35:00 +0300

    Базовая разметка сайта

---
+++ WebStore/Controllers/EmployesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployesController : Controller
+    {
+        private static List<EmployeeViewModel> __Employes = new List<EmployeeViewModel>
+        {
+            new EmployeeViewModel { Id = 0, FirstName = "Иван", SecondName = "Иванов", Patronymic = "Иванович", Age = 28 },
+            new EmployeeViewModel { Id = 0, FirstName = "Пётр", SecondName = "Петров", Patronymic = "Петрович", Age = 35 },
+            new EmployeeViewModel { Id = 0, FirstName = "Сидор", SecondName = "Сидоров", Patronymic = "Сидорович", Age = 22 },
+        };
+
+        public IActionResult Index()
+        {
+            //ViewBag.Title = "";
+            //ViewData["Title"] = "123";
+            return View(__Employes);
+        }
+    }
+}
