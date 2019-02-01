95a799370d6cb1334fb7dfa6bd95e7f12537ca58
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-01 21:06:13 +0300

    Разметка основных страниц сайта

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,11 +9,29 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        
-
-        public IActionResult Index()
+        public HomeController()
         {
-            return View();
-        }  
+
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult Shop() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult CheckOut() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Blog() => View();
+
+        public IActionResult ErrorPage404() => View();
     }
 }
