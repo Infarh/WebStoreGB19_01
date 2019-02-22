46e4cca6d6ce65a009d2e5bf19ee15d24a2741c3
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-22 20:06:33 +0300

    Доработка области Администрирования сайта

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastucture.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly IProductData _ProductsData;
+
+        public HomeController(IProductData ProductsData)
+        {
+            _ProductsData = ProductsData;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult ProductList()
+        {
+            return View(_ProductsData.GetProducts());
+        }
+
+        public IActionResult Create() => View();
+
+        public IActionResult Edit(int Id) => View();
+
+        public IActionResult Details(int Id) => View();
+
+        public IActionResult Delete(int Id) => View();
     }
 }
