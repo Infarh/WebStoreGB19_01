9b646f3dd948fcd97867ea3bec5ddd48921c041c
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-02 18:15:43 +0300

    В базовую реализацию клиента добавлены методы Get, Post, Put, Delete для упращения реализации наследников

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace WebStore.Clients.Base
@@ -21,6 +23,51 @@ namespace WebStore.Clients.Base
             };
             _Client.DefaultRequestHeaders.Accept.Clear();
             _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        }   
+        }
+
+        protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
+
+        protected async Task<T> GetAsync<T>(string url, CancellationToken cancel = default(CancellationToken)) 
+            where T : new()
+        {
+            var response = await _Client.GetAsync(url, cancel);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<T>(cancel);
+            return new T();
+            //return default(T);
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T value) 
+            where T : new() => 
+            PostAsync(url, value).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(
+            string url, 
+            T value, 
+            CancellationToken cancel = default(CancellationToken))
+            where T : new()
+        {
+            var response = await _Client.PostAsJsonAsync(url, value, cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T value)
+            where T : new() =>
+            PutAsync(url, value).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(
+            string url,
+            T value,
+            CancellationToken cancel = default(CancellationToken))
+            where T : new() =>
+                (await _Client.PutAsJsonAsync(url, value, cancel))
+                .EnsureSuccessStatusCode();
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync(
+            string url,
+            CancellationToken cancel = default(CancellationToken)) =>
+            await _Client.DeleteAsync(url, cancel);
     }
 }
