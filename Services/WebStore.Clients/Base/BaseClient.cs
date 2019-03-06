5aca47e265c888671d96ac2678c0e7c167fe391f
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-06 21:23:22 +0300

    Доработка класса базового клиента - добавлена реализация интерфейса IDisposable

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+// ReSharper disable SettingNotFoundInConfiguration
+// ReSharper disable UnusedMethodReturnValue.Global
+// ReSharper disable RedundantAwait
 
 namespace WebStore.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected readonly HttpClient _Client;
 
@@ -32,42 +33,48 @@ namespace WebStore.Clients.Base
         {
             var response = await _Client.GetAsync(url, cancel);
             if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsAsync<T>(cancel);
+                return await response.Content
+                    .ReadAsAsync<T>(cancel)
+                    .ConfigureAwait(false);
             return new T();
-            //return default(T);
         }
 
-        protected HttpResponseMessage Post<T>(string url, T value) 
-            where T : new() => 
-            PostAsync(url, value).Result;
+        protected HttpResponseMessage Post<T>(string url, T value) => PostAsync(url, value).Result;
 
         protected async Task<HttpResponseMessage> PostAsync<T>(
             string url, 
             T value, 
             CancellationToken cancel = default(CancellationToken))
-            where T : new()
         {
-            var response = await _Client.PostAsJsonAsync(url, value, cancel);
+            var response = await _Client
+                .PostAsJsonAsync(url, value, cancel)
+                .ConfigureAwait(false);
             return response.EnsureSuccessStatusCode();
         }
 
         protected HttpResponseMessage Put<T>(string url, T value)
-            where T : new() =>
-            PutAsync(url, value).Result;
+            => PutAsync(url, value).Result;
 
         protected async Task<HttpResponseMessage> PutAsync<T>(
             string url,
             T value,
-            CancellationToken cancel = default(CancellationToken))
-            where T : new() =>
-                (await _Client.PutAsJsonAsync(url, value, cancel))
-                .EnsureSuccessStatusCode();
+            CancellationToken cancel = default(CancellationToken)) =>
+                (await _Client.PutAsJsonAsync(url, value, cancel)
+                    .ConfigureAwait(false))
+                    .EnsureSuccessStatusCode();
 
         protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
 
         protected async Task<HttpResponseMessage> DeleteAsync(
             string url,
             CancellationToken cancel = default(CancellationToken)) =>
-            await _Client.DeleteAsync(url, cancel);
+            await _Client.DeleteAsync(url, cancel)
+                .ConfigureAwait(false);
+
+        #region Implementation of IDisposable
+
+        void IDisposable.Dispose() => _Client.Dispose();
+
+        #endregion
     }
 }
