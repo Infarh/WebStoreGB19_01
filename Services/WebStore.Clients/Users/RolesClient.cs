c9505e97c553ac6b5ed671c83d8425a65d145acc
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-06 21:25:26 +0300

    Добавлен Клиент+Контроллер для работы с Ролями IRoleStore<IdentityRole>

---
+++ Services/WebStore.Clients/Users/RolesClient.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+
+namespace WebStore.Clients.Users
+{
+    public class RolesClient : BaseClient, IRoleStore<IdentityRole>
+    {
+        public RolesClient(IConfiguration configuration) : base(configuration) => ServiceAddress = "api/roles";
+
+
+        #region Implementation of IRoleStore<IdentityRole>
+
+        public async Task<IdentityResult> CreateAsync(IdentityRole role, CancellationToken cancel) =>
+            await (await PostAsync(ServiceAddress, role, cancel))
+                .Content
+                .ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+
+        public async Task<IdentityResult> UpdateAsync(IdentityRole role, CancellationToken cancel) =>
+            await (await PutAsync(ServiceAddress, role, cancel))
+                .Content
+                .ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+
+        public async Task<IdentityResult> DeleteAsync(IdentityRole role, CancellationToken cancel) =>
+            await (await PostAsync($"{ServiceAddress}/Delete", role, cancel))
+                .Content
+                .ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+
+        public async Task<string> GetRoleIdAsync(IdentityRole role, CancellationToken cancel) =>
+            await (await PostAsync($"{ServiceAddress}/GetRoleId", role, cancel))
+                .Content
+                .ReadAsAsync<string>(cancel)
+                .ConfigureAwait(false);
+
+        public async Task<string> GetRoleNameAsync(IdentityRole role, CancellationToken cancel) =>
+            await (await PostAsync($"{ServiceAddress}/GetRoleName", role, cancel))
+                .Content
+                .ReadAsAsync<string>(cancel)
+                .ConfigureAwait(false);
+
+        public async Task SetRoleNameAsync(IdentityRole role, string name, CancellationToken cancel)
+        {
+            role.Name = name;
+            await PostAsync($"{ServiceAddress}/SetRoleName/{name}", role, cancel);
+        }
+
+        public async Task<string> GetNormalizedRoleNameAsync(IdentityRole role, CancellationToken cancel) =>
+            await (await PostAsync($"{ServiceAddress}/GetnormalizedRoleName", role, cancel))
+                .Content
+                .ReadAsAsync<string>(cancel)
+                .ConfigureAwait(false);
+
+        public async Task SetNormalizedRoleNameAsync(IdentityRole role, string name, CancellationToken cancel)
+        {
+            role.NormalizedName = name;
+            await PostAsync($"{ServiceAddress}/SetNormalizedRoleName/{name}", role, cancel);
+        }
+
+        public async Task<IdentityRole> FindByIdAsync(string id, CancellationToken cancel) =>
+            await GetAsync<IdentityRole>($"{ServiceAddress}/FindById/{id}", cancel);
+
+        public async Task<IdentityRole> FindByNameAsync(string name, CancellationToken cancel) =>
+            await GetAsync<IdentityRole>($"{ServiceAddress}/FindByName/{name}", cancel);
+
+        #endregion
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/RolesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [ApiController, Route("api/[controller]"), Produces("application/json")]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<IdentityRole> _RoleStore; 
+
+        public RolesController(WebStoreContext db)
+        {
+            _RoleStore = new RoleStore<IdentityRole>(db);
+        }
+
+        [HttpPost]
+        public async Task<bool> CreateAsync(IdentityRole role)
+        {
+            return (await _RoleStore.CreateAsync(role)).Succeeded;
+        }
+
+        [HttpPut]
+        public async Task<bool> UpdateAsync(IdentityRole role)
+        {
+            return (await _RoleStore.UpdateAsync(role)).Succeeded;
+        }
+
+        [HttpPost("Delete")]
+        public async Task<bool> DeleteAsync(IdentityRole role)
+        {
+            return (await _RoleStore.DeleteAsync(role)).Succeeded;
+        }
+
+        [HttpPost("GetRoleId")]
+        public async Task<string> GetRoleIdAsync(IdentityRole role)
+        {
+            return await _RoleStore.GetRoleIdAsync(role);
+        }
+
+        [HttpPost("GetRoleName")]
+        public async Task<string> GetRoleNameAsync(IdentityRole role)
+        {
+            return await _RoleStore.GetRoleNameAsync(role);
+        }
+
+        [HttpPost("SetRoleName/{name}")]
+        public async Task SetRoleNameAsync(IdentityRole role, string name)
+        {
+            await _RoleStore.SetRoleNameAsync(role, name);
+        }
+
+        [HttpPost("GetNormalizedRoleName")]
+        public async Task<string> GetNormalizedRoleNameAsync(IdentityRole role)
+        {
+            return await _RoleStore.GetNormalizedRoleNameAsync(role);
+        }
+
+        [HttpPost("SetNormalizedRoleName/{name}")]
+        public async Task SetnormalizedRoleNameAsync(IdentityRole role, string name)
+        {
+            await _RoleStore.SetNormalizedRoleNameAsync(role, name);
+        }
+
+        [HttpGet("FindById/{id}")]
+        public async Task<IdentityRole> FindByIdAsync(string id)
+        {
+            return await _RoleStore.FindByIdAsync(id);
+        }
+
+        [HttpGet("FindByName/{name}")]
+        public async Task<IdentityRole> FindByNameAsync(string name)
+        {
+            return await _RoleStore.FindByNameAsync(name);
+        }
+    }
+}
