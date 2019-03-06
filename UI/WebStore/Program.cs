197834d6f7f072c843f083a7eae0a30e5a739a00
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-06 21:26:18 +0300

    Процесс инициализации БД перенесён из WebStore в WebStore.ServiceHosting

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using WebStore.DAL.Context;
-using WebStore.Services.Data;
 
 namespace WebStore
 {
@@ -12,26 +7,7 @@ namespace WebStore
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var db = services.GetRequiredService<WebStoreContext>();
-                    db.Initialize();
-                    services.InitializeIdentityAsync().Wait();
-                }
-                catch (Exception e)
-                {
-                    services.GetRequiredService<ILogger<Program>>()
-                        .LogError(e, "Ошибка инициализации контекста БД");    
-                }
-            }
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
