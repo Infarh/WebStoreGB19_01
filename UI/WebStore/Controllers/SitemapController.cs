5fb78ee5fb2dc8981c9b610320bc23129f835967
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-16 19:38:27 +0300

    Карта сайта

---
+++ UI/WebStore/Controllers/SitemapController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers
+{
+    public class SitemapController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public SitemapController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index()
+        {
+            var nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Index", "Home")),
+                new SitemapNode(Url.Action("Shop", "Catalog")),
+                new SitemapNode(Url.Action("BlogSingle", "Home")),
+                new SitemapNode(Url.Action("Blog", "Home")),
+                new SitemapNode(Url.Action("ContactUs", "Home")),
+            };
+
+            foreach (var section in _ProductData.GetSections())
+            {
+                if (section.ParentSection != null)
+                    nodes.Add(new SitemapNode(Url.Action("Shop", "Catalog", new { SectionId = section.Id })));
+            }
+
+            foreach (var brand in _ProductData.GetBrands())
+            {
+                nodes.Add(new SitemapNode(Url.Action("Shop", "Catalog", new { BrandId = brand.Id })));
+            }
+
+            foreach (var product in _ProductData.GetProducts(new ProductFilter()))
+            {
+                nodes.Add(new SitemapNode(Url.Action("ProductDetails", "Catalog", new { Id = product.Id })));
+            }
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
