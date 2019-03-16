8a6a79c9cc2f9cae06784c4da4f8440dcd5ec19e
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-16 17:56:57 +0300

    Таг-хелпер для отслеживания положения на сайте

---
+++ Common/WebStore.TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = ActiveRouteAttributeName)]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        public const string ActiveRouteAttributeName = "is-active-route";
+
+        private Dictionary<string, string> _RouteValues;
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValues
+        {
+            get => _RouteValues ?? (_RouteValues = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase));
+            set => _RouteValues = value;
+        }
+
+        [HtmlAttributeNotBound, ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            if (ShouldBeActive())
+                MakeActive(output);
+
+            output.Attributes.RemoveAll(ActiveRouteAttributeName);
+        }
+
+        private bool ShouldBeActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+            var currrent_controller = route_values["Controller"].ToString();
+            var current_action = route_values["Action"].ToString();
+
+            const StringComparison str_comp = StringComparison.CurrentCultureIgnoreCase;
+            if (Controller?.Equals(currrent_controller, str_comp) == false) return false;
+            if (Action?.Equals(current_action, str_comp) == false) return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput Output)
+        {
+            const string class_attribute_name = "class";
+            const string active_state = "active";
+            var class_attribute = Output.Attributes.FirstOrDefault(a => a.Name == class_attribute_name);
+            if (class_attribute is null)
+            {
+                class_attribute = new TagHelperAttribute(class_attribute_name, active_state);
+                Output.Attributes.Add(class_attribute);
+            }
+            else if (class_attribute.Value?.ToString().ToLower().Contains(active_state) != true)
+                Output.Attributes.SetAttribute(
+                    class_attribute_name,
+                    class_attribute.Value is null
+                        ? active_state
+                        : $"{class_attribute.Value} {active_state}");
+
+        }
+    }
+}
---
+++ Common/WebStore.TagHelpers/Service/KeyValuePairExtensions.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.Collections.Generic
+{
+    internal static class KeyValuePairExtensions
+    {
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> item, out TKey Key, out TValue Value)
+        {
+            Key = item.Key;
+            Value = item.Value;
+        }
+    }
+}
